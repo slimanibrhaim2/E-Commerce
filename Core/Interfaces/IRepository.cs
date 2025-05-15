2c54f903d6d95a92bdb2f55c2964e2e4595ee9ae
aboud426 <abdullahabod170@gmail.com>
2025-05-16 01:28:05 +0300

    Refactor repository interfaces: Deleted IRepositry and created IRepository with enhanced methods for data access. Added IUnitOfWork interface for transaction management.

---
+++ Core/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Abstraction;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(Guid id);
+
+    // for filtering/query
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+
+}
